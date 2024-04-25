13c027aa71ea97b9df2cf62602f710f804e72544
Fran Ochoa <contact@fran-ochoa.es>
2024-04-25 17:30:08 +0200

    Initial Commit

---
+++ RickMortyAPI/RickMortyAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickMortyAPIApp.swift
+//  RickMortyAPI
+//
+//  Created by Fran Ochoa on 25/4/24.
+//
+
+import SwiftUI
+
+@main
+struct RickMortyAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
